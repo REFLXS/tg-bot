60deb1a4494482a3fda476f977c3094ff91ef05d
Nikita <129417335+REFLXS@users.noreply.github.com>
2025-03-15 00:02:30 +0400

    add buttons

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import config
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    username = message.from_user.first_name
-    bot.reply_to(message, f"Hi {username}, I'm {username}!")
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+  keyboard = types.ReplyKeyboardMarkup(row_width=2)
+  button1 = types.KeyboardButton('Кнопка 1')
+  button2 = types.KeyboardButton('Кнопка 2')
+  keyboard.add(button1, button2)
+
+  bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard)
 
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+def handle_message(message):
+  if message.text == 'Кнопка 1':
+      bot.reply_to(message, 'Вы нажали на Кнопку 1')
+  elif message.text == 'Кнопка 2':
+      bot.reply_to(message, 'Вы нажали на Кнопку 2')
+  else:
+      bot.reply_to(message, 'Получено сообщение: ' + message.text)
 
 bot.polling()
