288617178e0e6c8721508de947e8ec69f61209ff
Nikita <129417335+REFLXS@users.noreply.github.com>
2025-03-15 23:21:43 +0400

    add sqlite

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import config
 import telebot
+import sqlite3
 from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands=['start'])
-def handle_start(message):
-  keyboard = types.ReplyKeyboardMarkup(row_width=2)
-  button1 = types.KeyboardButton('Кнопка 1')
-  button2 = types.KeyboardButton('Кнопка 2')
-  keyboard.add(button1, button2)
+def start(message):
+    conection = sqlite3.connect('schedulerbot.sql')
+    cursor = conection.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users (id int auto_increment PRIMARY KEY, name varchar(50))')
+    conection.commit()
+    cursor.close()
+    conection.close()
 
-  bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard)
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+    button1 = types.KeyboardButton('Кнопка 1')
+    button2 = types.KeyboardButton('Кнопка 2')
+    keyboard.add(button1, button2)
+
+    bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard)
+
+def get_username(message):
+    name = message.text.strip()
+    conection = sqlite3.connect('schedulerbot.sql')
+    cursor = conection.cursor()
+    cursor.execute(f'INSERT INTO users (name) VALUES ({name})')
+    conection.commit()
+    cursor.close()
+    conection.close()
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
